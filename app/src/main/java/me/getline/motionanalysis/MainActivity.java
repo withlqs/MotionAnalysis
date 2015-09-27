3e59380ca97b9b3eb3f1f5040fd61bd5ae0dd7f8
withlqs <withlqs@gmail.com>
2015-09-28 00:44:25 +0800

    finished statistic function

---
+++ app/src/main/java/me/getline/motionanalysis/MainActivity.java
@@ -0,0 +1,81 @@
+package me.getline.motionanalysis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.VelocityTracker;
+import android.widget.TextView;
+
+import static java.lang.Math.sqrt;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textview;
+    private VelocityTracker vTracker = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textview = (TextView)findViewById(R.id.textview);
+    }
+
+    private long eventCount;
+    private double meanVelocity;
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int action = event.getAction();
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                if (vTracker == null) {
+                    vTracker = VelocityTracker.obtain();
+                } else {
+                    vTracker.clear();
+                }
+                eventCount = 0;
+                meanVelocity = 0.0;
+                vTracker.addMovement(event);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                vTracker.addMovement(event);
+                vTracker.computeCurrentVelocity(1000);
+                double xVelocity = vTracker.getXVelocity();
+                double yVelocity = vTracker.getYVelocity();
+                double nowVelocity = sqrt(xVelocity*xVelocity+yVelocity*yVelocity);
+                meanVelocity += (nowVelocity-meanVelocity)/(eventCount+1);
+                eventCount++;
+                textview.setText("Now Event Count is "+eventCount);
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                //vTracker.recycle();
+                textview.setText("Now Event Count is "+eventCount+"\nMean Velocity is "+meanVelocity);
+                break;
+        }
+        //event.recycle();
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
